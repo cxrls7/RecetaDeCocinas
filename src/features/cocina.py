7600ba005b668884aa9a230a291ac6784c43f3cd
Carlos Molina <carlitos17abx@gmail.com>
2026-03-03 15:14:08 -0500

    Cree las funciones de cocina

---
+++ src/features/cocina.py
@@ -0,0 +1,41 @@
+def preparar_pollo_a_la_plancha(presentacion):
+    """"
+    Prepara el pollo segun la presentacion
+    """""
+    
+    if presentacion == "tiras":
+        descripcion = "Pollo a la plancha cortado en tiras"
+    else: 
+        descripcion = "Pollo a la plancha en presentacion normal"
+
+    return descripcion
+
+def preparar_salsa_cesar(pimienta_negra_molida):
+    """"
+    Prepara la salsa cesar.
+    """""
+    
+    salsa = { 
+        "sal": "al gusto",
+        "zumo de limon":"10 ml",
+        "pimienta_negra": False 
+    }
+
+    if pimienta_negra_molida:
+        salsa["pimienta_negra"] = True
+
+    return salsa
+
+
+def emplatado(receta, salsa, presentacion_pollo, ingredientes, pasos):
+    """
+    Construye el diccionario al final del plato
+    """
+
+    return {
+        "receta": receta,
+        "salsa": salsa,
+        "presentacion_pollo": presentacion_pollo,
+        "ingredientes": ingredientes,
+        "pasos": pasos
+    }
